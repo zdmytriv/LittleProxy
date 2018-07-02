b215fca307edd5b54225bcf5d9ac1d2312e6eef5
aslepakurov <a.slepakurov@gmail.com>
2018-07-02 16:00:22 +0300

    Reconfiguration for CircleCI. AWS credential unset

--- scripts/env.sh
+++ scripts/env.sh
@@ -6,6 +6,9 @@ mkdir -p ~/.aws
 touch ~/.aws/credentials
 
 echo "
+[default]
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 [vgs-dev]
 region = us-west-2
 role_arn = arn:aws:iam::883127560329:role/StageDeploy
