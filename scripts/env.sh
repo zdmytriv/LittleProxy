ef64bb4f5d2d3adf656ef4846e9b3f09e8727059
aslepakurov <a.slepakurov@gmail.com>
2018-07-03 15:13:16 +0300

    Reconfiguration for CircleCI. AWS test purposes

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,13 +3,13 @@
 mkdir -p ~/.aws
 
 echo "
-[default]
-region = us-west-2
-aws_access_key_id=$AWS_ACCESS_KEY_ID
-aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
-[vgs-dev]
+#[default]
+#region = us-west-2
+#aws_access_key_id=$AWS_ACCESS_KEY_ID
+#aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+[profile vgs-dev]
 region = us-west-2
 role_arn = arn:aws:iam::883127560329:role/StageDeploy
 source_profile = default
-" > ~/.aws/credentials
+" > ~/.aws/config
 
