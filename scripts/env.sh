73e62d10a45c089e17c16fe5b3ef294a63d5e999
aslepakurov <a.slepakurov@gmail.com>
2018-07-03 15:23:54 +0300

    Reconfiguration for CircleCI. AWS test purposes

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.aws
 
 echo "
-#[default]
-#region = us-west-2
+[default]
+region = us-west-2
 #aws_access_key_id=$AWS_ACCESS_KEY_ID
 #aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 [vgs-dev]
