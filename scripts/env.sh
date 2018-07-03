e20504d7fe1e24d170e322aaf1327b99f70919ef
aslepakurov <a.slepakurov@gmail.com>
2018-07-03 16:16:15 +0300

    Restored CircleCI 2 migration

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,11 +5,10 @@ mkdir -p ~/.aws
 echo "
 [default]
 region = us-west-2
-#aws_access_key_id=$AWS_ACCESS_KEY_ID
-#aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 [vgs-dev]
 region = us-west-2
 role_arn = arn:aws:iam::883127560329:role/StageDeploy
 source_profile = default
-" >> ~/.aws/credentials
-
+" > ~/.aws/credentials
