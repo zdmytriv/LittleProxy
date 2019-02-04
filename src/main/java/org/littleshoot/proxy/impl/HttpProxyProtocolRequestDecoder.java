2d6d2fa1bed384b17e61b36e24f104497fefa103
Zinovii Dmytriv <zdmytriv@gmail.com>
2019-02-04 19:24:54 +0200

    update

--- src/main/java/org/littleshoot/proxy/impl/HttpProxyProtocolRequestDecoder.java
+++ src/main/java/org/littleshoot/proxy/impl/HttpProxyProtocolRequestDecoder.java
@@ -12,11 +12,19 @@ public class HttpProxyProtocolRequestDecoder extends ChannelInboundHandlerAdapte
 
   public static final AttributeKey<String> SOURCE_IP_ATTRIBUTE = AttributeKey.valueOf("sourceIp");
 
-  // PROXY_STRING + single space + INET_PROTOCOL + single space + CLIENT_IP + single space + PROXY_IP + single space + CLIENT_PORT + single space + PROXY_PORT + "\r\n"
-  // Example: PROXY TCP4 198.51.100.22 203.0.113.7 35646 80\r\n
-  // Source: https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/enable-proxy-protocol.html
-  private static final Pattern PROXY_PROTOCOL_HEADER_PATTERN =
-      Pattern.compile("PROXY TCP4 (((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.)?){4}) (((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.)?){4}) \\d+ \\d+");
+  // Pattern:
+  //   PROXY_STRING + single space + INET_PROTOCOL + single space + CLIENT_IP + single space + PROXY_IP + single space + CLIENT_PORT + single space + PROXY_PORT + "\r\n"
+  // Example:
+  //   PROXY TCP4 198.51.100.22 203.0.113.7 35646 80\r\n
+  //   or
+  //   PROXY TCP6 2001:DB8::21f:5bff:febf:ce22:8a2e 2001:DB8::12f:8baa:eafc:ce29:6b2e 35646 80
+  // Source:
+  //   https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/enable-proxy-protocol.html
+  private static final Pattern TCP4_PROXY_PROTOCOL_HEADER_PATTERN =
+      Pattern.compile("PROXY TCP4 (((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.)?){4}) ((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.)?){4} \\d+ \\d+\\r\\n");
+
+  private static final Pattern TCP6_PROXY_PROTOCOL_HEADER_PATTERN =
+      Pattern.compile("PROXY TCP6 (([0-9a-f]{1,4}:){7}([0-9a-f]){1,4}) ([0-9a-f]{1,4}:){7}([0-9a-f]){1,4} \\d+ \\d+\\r\\n", Pattern.CASE_INSENSITIVE);
 
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -24,14 +32,20 @@ public class HttpProxyProtocolRequestDecoder extends ChannelInboundHandlerAdapte
 
     String body = bufToString(buf);
 
-    Matcher matcher = PROXY_PROTOCOL_HEADER_PATTERN.matcher(body);
+    Matcher tcp4Matcher = TCP4_PROXY_PROTOCOL_HEADER_PATTERN.matcher(body);
+    Matcher tcp6Matcher = TCP6_PROXY_PROTOCOL_HEADER_PATTERN.matcher(body);
 
-    if (!matcher.find()) {
+    String sourceIp;
+
+    if (tcp4Matcher.find()) {
+      sourceIp = tcp4Matcher.group(1);
+    } else if (tcp6Matcher.find()) {
+      sourceIp = tcp6Matcher.group(1);
+    } else {
       ctx.fireChannelRead(msg);
       return; // no proxy protocol header found, proceed
     }
 
-    String sourceIp = matcher.group(1);
     ctx.channel().attr(SOURCE_IP_ATTRIBUTE).set(sourceIp);
 
     int proxyProtocolHeaderIndex = body.indexOf("\r\n");
@@ -39,14 +53,14 @@ public class HttpProxyProtocolRequestDecoder extends ChannelInboundHandlerAdapte
 
     buf.clear().writeBytes(stripped.getBytes());
 
-    ctx.fireChannelRead(msg);
+    ctx.fireChannelRead(buf);
   }
 
   private String bufToString(ByteBuf content) {
-    final byte[] bodyBytes = new byte[content.readableBytes()];
+    final byte[] bytes = new byte[content.readableBytes()];
 
-    content.getBytes(content.readerIndex(), bodyBytes);
+    content.getBytes(content.readerIndex(), bytes);
 
-    return new String(bodyBytes);
+    return new String(bytes);
   }
 }
